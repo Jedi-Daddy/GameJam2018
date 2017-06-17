8b83ddfd5d8a9b2314d8e1d022171016e48a8e4b
Ruben Markov <r.markov@plarium.com>
2017-06-17 16:08:37 +0300

    remove test prefabs and add base ui scripts

---
+++ Jam/Assets/Scripts/ViewModel/GameUIView.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Scripts.ViewModel
+{
+  public class GameUIView : MonoBehaviour
+  {
+    private GameState _currentState;
+    private int _curentPlayerId;
+    private int _maxHp;
+
+    public HeroInfoView[] HeroesInfo;
+    public Slider HpProgressBarSlider;
+    public Text TimerText;
+    public Text EnergyPointsText;
+
+    public void SetState(GameState state)
+    {
+      _currentState = state;
+      _curentPlayerId = _currentState.CurrentPlayer.Id;
+
+      SetHpValue(_currentState.Heroes[_curentPlayerId].HitPoints);
+      SetEnergyPointsCount(_currentState.CurrentPlayer.ActionPoints);
+    }
+    
+    public void SetHpValue(int currentHP)
+    {
+      var value = (float) currentHP/_maxHp;
+
+      HpProgressBarSlider.value = value;
+    }
+
+    public void SetTimerValue(string timerValue)
+    {
+      TimerText.text = timerValue;
+    }
+
+    public void SetEnergyPointsCount(int points)
+    {
+      EnergyPointsText.text = points.ToString();
+    }
+  }
+}
---
+++ Jam/Assets/Scripts/ViewModel/HeroInfoView.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Scripts.ViewModel
+{
+  public class HeroInfoView : MonoBehaviour
+  {
+    private int _maxHP;
+    public Slider HPProgressBarSlider;
+    
+    public void Initialize(int maxHP)
+    {
+      _maxHP = maxHP;
+    }
+
+    public void UpdateHPValue(int currentHP)
+    {
+      var value = (float)currentHP / _maxHP;
+      HPProgressBarSlider.value = value;
+    }
+  }
+}
