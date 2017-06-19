b6cbbccc76850c7d70c4b03e8d0cdbe670784288
Ruben Markov <r.markov@plarium.com>
2017-06-19 17:53:06 +0300

    Esc "manager"

---
+++ Jam/Assets/Scripts/EscManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EscManager : MonoBehaviour
+{
+	void Update () 
+  {
+		if(Input.GetKeyUp(KeyCode.Escape))
+	  {
+	    SceneManager.LoadScene(0);
+	  }
+	}
+}
